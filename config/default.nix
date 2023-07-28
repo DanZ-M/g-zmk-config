e3c710cbcca5caac6cd24253716dc39d8697a4a2
DanZ-M <118416972+DanZ-M@users.noreply.github.com>
2023-07-28 13:44:58 +1000

    Update default.nix

--- config/default.nix
+++ config/default.nix
@@ -6,5 +6,3 @@ let
 
   glove80_left  = firmware.zmk.override { board = "glove80_lh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
   glove80_right = firmware.zmk.override { board = "glove80_rh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
-
-in firmware glove80_left glove80_right
